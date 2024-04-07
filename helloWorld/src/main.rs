89379887030d1e67a5b1d2cb9e1110805e10b7a6
Samyak-J7 <sj21samyak@gmail.com>
2024-04-07 10:54:13 +0530

    first commit

---
+++ helloWorld/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("Hello, world!");
+    first_fn();
+}
+fn first_fn () {
+    println!("Lets get Rusty!");
+}
---
+++ variables/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Learning variables in Rust!");
+    let name = "Samyak";
+    let lastname = "Jain";
+    println!("{name} {lastname}")
+
+}
+
